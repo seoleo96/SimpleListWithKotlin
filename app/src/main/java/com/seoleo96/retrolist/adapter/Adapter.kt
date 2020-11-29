cec9d5c1910f47819e20a0e9c61d423d5f3cb1ee
user <seoleo96@gmail.com>
2020-11-29 22:59:37 +0600

    created bind method on ViewHolder class

--- app/src/main/java/com/seoleo96/retrolist/adapter/Adapter.kt
+++ app/src/main/java/com/seoleo96/retrolist/adapter/Adapter.kt
@@ -1,18 +1,10 @@
 package com.seoleo96.retrolist.adapter
 
-import android.content.Intent
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.seoleo96.retrolist.Main2Activity
 import com.seoleo96.retrolist.R
 import com.seoleo96.retrolist.model.Teacher
-import kotlinx.android.synthetic.main.row_teacher.view.*
-import java.lang.Thread.sleep
-import kotlin.concurrent.thread
 
 class Adapter(val teacherlist : List<Teacher>?) : RecyclerView.Adapter<ViewHolder>() {
 
@@ -24,20 +16,7 @@ class Adapter(val teacherlist : List<Teacher>?) : RecyclerView.Adapter<ViewHolde
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = holder.itemView
-        item.textView.text = teacherlist?.get(position)?.name
-        Glide.with(item.context).asBitmap().load(teacherlist?.get(position)?.img).into(item.circleImageView)
-
-        holder.itemView.setOnClickListener {
-            sleep(2000)
-            Toast.makeText(holder.itemView.context, teacherlist?.get(position)?.name.toString(), Toast.LENGTH_SHORT).show()
-            val intent = Intent(holder.itemView.context, Main2Activity::class.java)
-            intent.putExtra("name", teacherlist?.get(position)?.name)
-            intent.putExtra("img", teacherlist?.get(position)?.img)
-            holder.itemView.context.startActivity(intent)
-
-        }
-
+        teacherlist?.get(position)?.let { holder.bind(it) }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/seoleo96/retrolist/adapter/ViewHolder.kt
+++ app/src/main/java/com/seoleo96/retrolist/adapter/ViewHolder.kt
@@ -1,6 +1,27 @@
 package com.seoleo96.retrolist.adapter
 
+import android.content.Intent
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.seoleo96.retrolist.Main2Activity
+import com.seoleo96.retrolist.model.Teacher
+import kotlinx.android.synthetic.main.row_teacher.view.*
 
-class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+open class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+{
+    fun bind(teacherlist: Teacher) {
+        itemView.textView.text = teacherlist?.name
+        Glide.with(itemView.context).asBitmap().load(teacherlist?.img).into(itemView.circleImageView)
+
+        itemView.setOnClickListener {
+            Thread.sleep(1000)
+            Toast.makeText(itemView.context, teacherlist?.name.toString(), Toast.LENGTH_SHORT).show()
+            val intent = Intent(itemView.context, Main2Activity::class.java)
+            intent.putExtra("name", teacherlist?.name)
+            intent.putExtra("img", teacherlist?.img)
+            itemView.context.startActivity(intent)
+        }
+    }
+}
